af4c8eb38effe01dcd7209ddf09f3af0a377c0e8
Heming <tihe@bfw-dortmund.de>
2025-11-19 12:53:20 +0100

    fixed bug in spielerZumKreis

--- ZippZapp/Spielkreis.cs
+++ ZippZapp/Spielkreis.cs
@@ -24,11 +24,21 @@ namespace ZippZapp
 				start_spieler.spieler_links = start_spieler;
 				start_spieler.spieler_rechts = start_spieler;
 			} else {
-				// TODO[2]: Zeichnen für den Zustand vor der Ausführung der folgenden drei Codezeilen jeweils ein Objektdiagramm.
+				// Zeichnen für den Zustand vor der Ausführung der folgenden drei Codezeilen jeweils ein Objektdiagramm.
 				// Beschreiben sie mit dem Diagramm einen Zustand, der während der Ausführung der Methode erzeugeTestKreis() auftreten kann.
+				
+				//Aufgezeichnet für den Fall: Ein Spieler im Kreis, einer neu dazu -> Fehler korrigiert unter 4
+				//TODO[2]: Funktioniert das auch für 2+1?
+				
+				
+				//1
 				neuerSpieler.spieler_links = start_spieler.spieler_links;
+				//2
 				start_spieler.spieler_links = neuerSpieler;
-				neuerSpieler.spieler_rechts = start_spieler;			
+				//3
+				neuerSpieler.spieler_rechts = start_spieler;
+				//4
+				start_spieler.spieler_rechts = neuerSpieler;
 			}
 		}
 		
